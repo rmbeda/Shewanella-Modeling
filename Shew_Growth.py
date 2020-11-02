ab58c324edce4821642743a868cc53bcb998b0f7
rmbeda <52925556+rmbeda@users.noreply.github.com>
2020-11-01 17:51:20 -0800

    Nov 1 Update
    Removed Paul author credit
    Updated rate functions with afdw conversion and m^2 factor in mass change

--- Shew_Growth.py
+++ Shew_Growth.py
@@ -1,5 +1,4 @@
-# Author Paul Juralowicz
-# Last Modified by Robert Beda on September October 4, 2020
+# Last Modified by Robert Beda on September November 1, 2020
 # Modelling Growth of Shewanella Oneidensis
 # Assume linear relation of 0.00441mol/(gram*hour) consumed per 0.085 doublings/hour
 
@@ -10,34 +9,34 @@ import matplotlib.pyplot as plt
 # Robert, remember to run using Python3 in order to avoid weird 'infinite loop' error
 # Before running, vary initial conditions and time-step info as desired:
 
-V = 0.775/2 # Assumed volume of one half-cell (the one with the bacteria)
-m0 = 0.001 # g
-lc0 = 0.01 # mol (= ~0.9 g)
-tmax = 1000 # Number of hours over which to run the simulation
-steps = 20000 # Number of time steps to use
-
+V = 0.775/2 # L - Assumed volume of one half-cell (the one with the bacteria)
+m0 = 0.03 # g - Initial bacterial population mass
+lc0_m = 0.05
+lc0 = 5 # mol (= ~0.9 g) - Initial moles of lactate
+tmax = 72 # Number of hours over which to run the simulation
+steps = 7200 # Number of time steps to use
+print(lc0)
 # For non-debugging use, don't edit past here.
 
-grow = 0.085/0.00441 # grams of bacteria per mol of lactate consumed
 molm = 90.08 # molar mass of lactate (g/mol)
+grow = 0.085/0.00441 # doublings/hour per mol of lactate consumed/hour
+asfd_conv = 0.22 # grams of bacteria per mL of bacterial volume
 def l_per_gbac(l):
 	"""Moles of Lactate consumed per gram of bacteria per hour"""
-	return l/(1000*V)
+	return l/(1000*V)*(asfd_conv)**(-1)
 
-
-def dm(m, l):
-	# Rate of change in mass of bacteria (grams)
-	return grow*l_per_gbac(l)*m
-
-def dl(m, l):
+def dldt(m, l):
 	# Rate of change in amount of lactate (moles)
 	return -l_per_gbac(l)*m
 
-l0 = lc0 # line available to convert lactate calcs to grams
-Ys0 = np.array([m0, l0]) # initial conditions for odeint
+def dmdt(m, l):
+	# Rate of change in mass of bacteria (grams)
+	return -grow*dldt(m, l)*m
+
+Ys0 = np.array([m0, lc0]) # initial conditions for odeint
 
 dt = tmax/steps # Time differential between time steps
-ts = np.arange(steps+1)*dt # Array of all times to be represented in plots.
+ts = np.arange(steps)*dt # Array of all times to be represented in plots.
 
 
 def stepping(inits, ts):
@@ -56,7 +55,7 @@ def stepping(inits, ts):
 	"""
 	m = inits[0]
 	l = inits[1]
-	return np.array([dm(m, l), dl(m, l)])
+	return np.array([dmdt(m, l), dldt(m, l)])
 
 
 masses = integrate.odeint(stepping, Ys0, ts)
@@ -67,8 +66,8 @@ plt.xlabel("Time (Hours)")
 plt.ylabel("Mass (g)")
 plt.title("Bacteria")
 plt.show()
-plt.plot(ts, ls, "b", label = "Lactate Amount")
+plt.plot(ts, ls, "b", label = "Lactate Moles")
 plt.xlabel("Time (Hours)")
-plt.ylabel("Moles")
-plt.title("Lactate")
+plt.ylabel("Lactate (Moles)")
+plt.title("Lactate in Solution")
 plt.show()
